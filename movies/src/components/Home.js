6ecda66c942dff6cddeaabd5d19043500a330089
kgydilmith <dilmithyesitha@gmail.com>
2024-09-06 15:54:41 +0530

    movie item

--- movies/src/components/Home.js
+++ movies/src/components/Home.js
@@ -1,8 +1,18 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import Movieitem from "./Movies/Movieitem";
+import { Link } from "react-router-dom";
+import { getallmovies } from "../api-helpers/api-helpers.js";
 
 const Home = () => {
+  const [movies, setmovies] = useState([]);
+  useEffect(() => {
+    getallmovies()
+      .then((data) => setmovies(data.movies))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(movies);
+
   return (
     <Box width={"100%"} height="100%" margin="auto" marginTop={2}>
       <Box margin={"auto"} width="80%" height={"50vh"} padding={2}>
@@ -18,13 +28,25 @@ const Home = () => {
           Leatest Releases
         </Typography>
       </Box>
-      <Box display="flex"
-      width="80%"
-      justifyContent={"center"}
-      flexWrap="wrap">
-         {[1,2,3,4].map((item) => (
-          <Movieitem/>
-         ))}
+      <Box
+        display="flex"
+        width="100%"
+        justifyContent={"center"}
+        flexWrap="wrap"
+      >
+        {[1, 2, 3, 4].map((item) => (
+          <Movieitem key={item} />
+        ))}
+      </Box>
+      <Box display="flex" padding={5} margin="auto">
+        <Button
+          LinkComponent={Link}
+          to="/movies"
+          variant="outlined"
+          sx={{ margin: "auto", color: "#2b2d42" }}
+        >
+          View all movies{" "}
+        </Button>
       </Box>
     </Box>
   );
--- movies/src/components/Movies/Movieitem.js
+++ movies/src/components/Movies/Movieitem.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent"; 
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-const Movieitem = () => {
+const Movieitem = ({title,releaseData,posterurl,id }) => {
   return (
-    <Card sx={{margin:5,width:250,height:320,borderRadius:5,":hover":{
-        boxShadow:"10px 10px 20px #ccc",
-    }}}>
+    <Card
+      sx={{
+        margin:2,
+        width: 250,
+        height: 320,
+        borderRadius: 5,
+        ":hover":{
+          boxShadow: "10px 10px 20px #ccc",
+        }
+      }}
+    >
+      <img height={'35%'} width={'100%'} src={posterurl} alt={title}/>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+         {title}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {new Date(releaseData).toDateString()}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button sx={{margin:"auto"}} size="small">Share</Button>
+      </CardActions>
+    </Card>
+  );
+};
 
-      <img height={'50%'} width='100%' src="" alt=''/>  
-    
-    <CardContent>
-      <Typography gutterBottom variant="h5" component="div">
-        Lizard
-      </Typography>
-      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-        Lizards are a widespread group of squamate reptiles, with over 6,000
-        species, ranging across all continents except Antarctica
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button sx={{margin:"auto"}} size="small">Share</Button>
-       
-    </CardActions>
-  </Card> 
-  )
-}
-
-export default Movieitem
+export default Movieitem;
